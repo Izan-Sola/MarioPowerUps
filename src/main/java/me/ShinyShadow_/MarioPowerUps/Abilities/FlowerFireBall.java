ef97a7e74811603e50ed6a10bf4482a59018f48f
Izan Sola Montesinos <83542827+ShinyShadow12@users.noreply.github.com>
2025-01-14 08:41:36 +0100

    Update FlowerFireBall.java

--- src/main/java/me/ShinyShadow_/MarioPowerUps/Abilities/FlowerFireBall.java
+++ src/main/java/me/ShinyShadow_/MarioPowerUps/Abilities/FlowerFireBall.java
@@ -47,18 +47,20 @@ public class FlowerFireBall {
             @Override
             public void run() {
                 if (t > 1.0) {
-                    //Check if block under is air, if it is, keep falling until touching ground and bounce
+
                 if (bounceCount == 2) {
                     fireBallPather.getWorld().spawnParticle(Particle.FLAME, fireBallPather.getLocation(), 120, 0.11, 0.11, 0.11, 0.06);
                     fireBallPather.getWorld().spawnParticle(Particle.SMOKE, fireBallPather.getLocation(), 160, 0.11, 0.11, 0.11, 0.07);
                     fireBallPather.getWorld().spawnParticle(Particle.FALLING_LAVA, fireBallPather.getLocation(), 62, 0.8, 0.6, 0.8, 1.8);
                     player.playSound(fireBallPather.getLocation(), Sound.BLOCK_LAVA_EXTINGUISH, 0.3f, 1.8f);
 
-                    player.sendMessage(endPoint.getBlock().getLocation()+"AAAAAAAAAAAAAAAAAAAA"+fireBallPather.getLocation());
+                   // player.sendMessage(endPoint.getBlock().getLocation()+"AAAAAAAAAAAAAAAAAAAA"+fireBallPather.getLocation());
                     fireBallPather.remove();
                     cancel();
                     return;
                 }
+                    //Check if the block under is passable, if it is, keep falling until touching the ground and set
+                    //new start, curve and end points
                     if (!endPoint.getBlock().getType().isSolid()) {
                         currentLocation = fireBallPather.getLocation().add(0, -0.5, 0);
                         fireBallPather.teleport(currentLocation);
@@ -92,10 +94,10 @@ public class FlowerFireBall {
                         if (entity instanceof LivingEntity && entity.getUniqueId() != player.getUniqueId()) {
                             entity.setFireTicks(80);
                             ((LivingEntity)entity).damage(6);
+        
                             t=2;
                         }
-                    }
-
+                    
                     if(!fireBallPather.getLocation().getBlock().isPassable()){
                         t=2;
                     }
@@ -111,7 +113,7 @@ public class FlowerFireBall {
         fireBallPather.getWorld().spawnParticle(Particle.SMOKE, fireBallPather.getLocation(), 18, 0.2, 0.2, 0.2, 0.2);
         fireBallPather.getWorld().spawnParticle(Particle.FALLING_LAVA, fireBallPather.getLocation(), 12, 0.2, 0.2, 0.2, 1.8);
 
-       player.playSound(fireBallPather.getLocation(), Sound.BLOCK_LAVA_EXTINGUISH, 0.1f, 4f);
+        player.playSound(fireBallPather.getLocation(), Sound.BLOCK_LAVA_EXTINGUISH, 0.1f, 4f);
         player.getWorld().playSound(player.getLocation(), Sound.ENTITY_BLAZE_SHOOT, 0.1f, 3.4f);
 
         Block touchedBlock = fireBallPather.getLocation().getBlock();
@@ -119,7 +121,7 @@ public class FlowerFireBall {
         double offSetY = touchedBlock.getY();
 
         bounceDistance += 4;
-
+        //Prevent endpoint from being out of bounds withing the line of sight range
         if(bounceDistance > lineOfSight.size()-1) {
             bounceDistance = lineOfSight.size() - 1;
             lastBlock = lineOfSight.getLast().getLocation();
@@ -128,17 +130,24 @@ public class FlowerFireBall {
             lastBlock = lineOfSight.get(bounceDistance).getLocation();
         }
         bounceCount+=1;
+        
+        //If the Y level of the endpoint is different from the Y of the start point (i.e: you throw the fireball from a cliff)
+        //substract the Y value of the endpoint to itself (i.e: lastblock Y = 60, so 60-60 = 0Y)
+        //then add the Y of the new startpoint, (which would be the floor level, the block the fireball touched)
+        //(i.e now the endpoint Y += start point Y --> 0 + 40 = 40Y) so now both the startpoint and the endpoint are at the same Y level.
+        //Then just add a bit of height to the curve (40 + 2 0 42Y) and done.
+        
         startPoint = fireBallPather.getLocation().getBlock().getLocation();
         endPoint = lastBlock.add(0, (-lastBlock.getY())+offSetY, 0);
 
+        //In case the endpoint, being at the same Y level as the startpoint is NOT a passable block (meaning stairs, uneven terrain...)
+        //it adds +1 to the Y level of the endpoint, so it can jump over different Y levels, unless theres a tall wall,
+        //since it only adds +1 to the Y level one time, it wont go over them, but instead collide and explode
+        
             if(!endPoint.getBlock().getLocation().add(0, 0, 0).getBlock().isPassable()) {
                 endPoint = lastBlock.add(0, (-lastBlock.getY())+offSetY+1, 0);
                 idkbro = 1;
             }
-
-
-
-
 
         double test = lineOfSight.get(bounceDistance-1).getLocation().getY();
         curvePoint = lineOfSight.get(bounceDistance-1).getLocation().add(0, (-test)+offSetY+bounceHeightModifier, 0);
