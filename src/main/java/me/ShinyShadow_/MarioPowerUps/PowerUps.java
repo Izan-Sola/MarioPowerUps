cf80eb38a6099fd76c8a97ff5be11cba69dbb42e
Izan Sola Montesinos <83542827+ShinyShadow12@users.noreply.github.com>
2025-01-15 12:11:41 +0100

    Update PowerUps.java

--- src/main/java/me/ShinyShadow_/MarioPowerUps/PowerUps.java
+++ src/main/java/me/ShinyShadow_/MarioPowerUps/PowerUps.java
@@ -16,7 +16,15 @@ public final class PowerUps extends JavaPlugin {
 		getCommand("givefireflower").setExecutor(new Commands());
 
 		Bukkit.getPluginManager().registerEvents(new FireFlowerListener(this), this);
-		
+
+//Either hard to obtain permanent uses or easy to obtain but it is consumed/destroyed or needs to be recharged.
+//If it has permanent uses i will make a craftable custom item  (mario related too) for the recipe to make it harder to craft and obtain
+//If it needs to be recharged, it will also need a custom item for recharge. Maybe after 3 uses of the item it runs out of power and u need to recharge it.
+
+
+
+
+		//ignore this recipe
 	//	Bukkit.getPluginManager().registerEvents(new ItemListener(this), this);
 		
 //		ShapedRecipe recipe = new ShapedRecipe(new NamespacedKey(this, "PowerUps"), ItemManager.Fire_Flower);
